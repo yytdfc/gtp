8669cf330960b0bec5f550864d8929fb53040368
James Tauber <jtauber@jtauber.com>
2016-03-16 21:26:48 -0400

    added setup.py for release to PyPI
    unfortunately had to use pygtp rather than gtp as latter was taken

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="pygtp",
+    version="0.1",
+    description="Python library for Go Text Protocol controllers and engines",
+    license="MIT",
+    url="http://github.com/jtauber/gtp",
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    py_modules=["gtp"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Games/Entertainment :: Board Games",
+        "Topic :: Utilities",
+    ],
+)
